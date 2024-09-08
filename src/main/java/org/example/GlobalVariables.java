561f858ceb68885993135aefd60652a89fd26ff2
pawelleszczuk <141743586+pawelleszczuk@users.noreply.github.com>
2024-09-09 00:17:26 +0200

    Delete src/main/java/org/example directory

--- src/main/java/org/example/GlobalVariables.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class GlobalVariables {
-    public static List<Person> people = new ArrayList<>();
-    public static String sqlReadInstruction;
-}
